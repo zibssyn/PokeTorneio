bd41d06b4d9f9f3df8e5547b420226d6b9c0a57b
zibssyn <gricvie@gmail.com>
2024-09-24 21:52:32 -0300

    Alterações para funcionar o banco de dados funcionou certinho em produção

--- obj/Debug/net8.0/PokeTorneio.AssemblyInfo.cs
+++ obj/Debug/net8.0/PokeTorneio.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeTorneio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a09e52dd0b875a2ab6c6b74fa385552320e7b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeTorneio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeTorneio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
